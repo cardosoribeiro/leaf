c66d08f1329a303a22523acee032d6624c84479b
cardosoribeiro <cardosoribeiro.mail@gmail.com>
2025-02-12 01:03:47 -0300

    adds about text

--- database/create_script_0.1.sql
+++ database/create_script_0.1.sql
@@ -4,10 +4,10 @@ USE Leaf;  -- Selects the Leaf database for use
 
 CREATE TABLE User (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_role ENUM('administrator', 'doctor') NOT NULL,
+    role ENUM('ADMINISTRATOR', 'DOCTOR') NOT NULL,
     full_name VARCHAR(255) NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     email VARCHAR(255),
     is_active BOOLEAN DEFAULT TRUE
 );
